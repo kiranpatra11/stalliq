01f635a40c3149c9155c3711c0a6df29f5e9eeac
kiranpatra11 <kiranpatra199@gmail.com>
2025-09-12 18:15:43 +0530

    functions

---
+++ Play ground/functions.js
@@ -0,0 +1,7 @@
+function name (name){
+    console.log("hey " + name + " your roll is 43");
+    console.log("hey " + name + " you live in bengal");
+    console.log("hey " + name + " you are a good");
+    console.log("hey " + name + " kiran loves you a lot");
+}
+name("Astha");
